54ac0dfabb419e161aacf0e707bc9aec31802ef0
24LuisLeal <71896945+24LuisLeal@users.noreply.github.com>
2021-01-22 18:27:56 -0600

    Add files via upload

---
+++ CameraLook.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineFreeLook))]
+
+public class CameraLook : MonoBehaviour
+{
+    private CinemachineFreeLook cinemachine;
+    private InputMovement inputPlayer;
+    private float lookSpeed = 1f;
+
+    private void Awake()
+    {
+        inputPlayer = new InputMovement();
+        cinemachine = GetComponent<CinemachineFreeLook>();
+    }
+
+    private void OnEnable()
+    {
+        inputPlayer.Enable();
+    }
+    private void OnDisable()
+    {
+        inputPlayer.Disable();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 delta = inputPlayer.PlayerMain.Look.ReadValue<Vector2>();
+        cinemachine.m_XAxis.Value += delta.x * 200 *  lookSpeed * Time.deltaTime;
+        cinemachine.m_YAxis.Value += delta.y * 200 * lookSpeed * Time.deltaTime;
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Referencia al script del movimiento
+    private InputMovement inputPlayer;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    private Transform cameraMain;
+    private Transform child;
+    private float rotationSpeed = 4f;
+
+    private void Awake()
+    {
+        inputPlayer = new InputMovement();
+        controller = GetComponent<CharacterController>();
+    }
+    private void OnEnable()
+    {
+        inputPlayer.Enable();
+    }
+    private void OnDisable()
+    {
+        inputPlayer.Disable();
+    }
+    private void Start()
+    {
+        //Ver donde el jugador está viendo
+        cameraMain = Camera.main.transform;
+        child = transform.GetChild(0).transform;
+    }
+
+    void Update()
+    {
+        //Esta el jugador en el suelo si o no
+        groundedPlayer = controller.isGrounded;
+        //Si está en el suelo y la velocidad en y es mayor a 0
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            //Entonces no habrá ninguna fuerza en y
+            playerVelocity.y = 0f;
+        }
+        //Movimiento en el vector en 2d Joysticks
+        Vector2 movementInput = inputPlayer.PlayerMain.Move.ReadValue<Vector2>();
+        //Movimiento en el plano "x" y "y"
+        Vector3 move = (cameraMain.forward * movementInput.y + cameraMain.right * movementInput.x);
+        move.y = 0f;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        //Si el botón de saltar está siendo oprimido y el jugador está en el suelo
+        if (inputPlayer.PlayerMain.Jump.triggered && groundedPlayer)
+        {
+            //Entonces saltaremos
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if(movementInput != Vector2.zero)
+        {
+            Quaternion rotation = Quaternion.Euler(new Vector3
+            (child.localEulerAngles.x, cameraMain.localEulerAngles.y, child.localEulerAngles.z));
+            child.rotation = Quaternion.Lerp(child.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+}
